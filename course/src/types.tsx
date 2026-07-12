1417daec7c29888ff0e44f993dae4a416d837db5
Khoi <khoidang01234@gmail.com>
2026-07-12 14:08:55 +0700

    add typing course

---
+++ course/src/types.tsx
@@ -0,0 +1,35 @@
+export interface CoursePartBase {
+  name: string;
+  exerciseCount: number;
+}
+
+export interface CoursePartDescription extends CoursePartBase {
+  description: string;
+}
+
+export interface CoursePartBasic extends CoursePartBase, CoursePartDescription {
+  kind: "basic";
+}
+
+export interface CoursePartGroup extends CoursePartBase {
+  groupProjectCount: number;
+  kind: "group";
+}
+
+export interface CoursePartBackground
+  extends CoursePartBase, CoursePartDescription {
+  backgroundMaterial: string;
+  kind: "background";
+}
+
+export interface CoursePartRequirement
+  extends CoursePartBase, CoursePartDescription {
+  requirements: string[];
+  kind: "special";
+}
+
+export type CoursePart =
+  | CoursePartBasic
+  | CoursePartGroup
+  | CoursePartBackground
+  | CoursePartRequirement;
